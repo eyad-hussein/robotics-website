39010a653c3f9d123679d38f2fb02816bbb05814
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-01-07 06:57:26 +0200

    Retrieve images to main carousel

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,12 +3,25 @@ import CarouselCounter from "../../components/carousel-counter/carousel-counter.
 import Posts from "../../components/posts/posts.component";
 import Footer from "../../components/footer/footer.component";
 import { Fragment } from "react";
+import { api } from "../../services/http_request";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [carouselImages, setCarouselImages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await api.get(`images/active`);
+      const result = response.data;
+      setCarouselImages(result.mainCarouselImages);
+    };
+    fetchData();
+  }, []);
   return (
     <Fragment>
       <main className='main u-margin-bottom-big'>
-        <CarouselCounter />
+        {carouselImages.length && <CarouselCounter images={carouselImages} />}
+
         <Posts />
       </main>
 
